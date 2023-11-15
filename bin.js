9cd9f63220ac359c0d92f18776d4e9aabccbbfe1
GibsDev <notcurtisg@gmail.com>
2023-11-15 11:46:34 -0500

    implement functionality

--- bin.js
+++ bin.js
@@ -1,6 +1,18 @@
+#!/usr/bin/env node
+
 /**
  * @file This is what gets run when a user npx's this module
  */
 
-// TODO get stuff from bitwarden
+import path from 'path';
+import fetchSecrets from './lib/fetch-secrets.js';
 
+// Get root path from arguments or use current working directory
+let root = process.cwd();
+if (process.argv[2]) {
+    root = path.resolve(process.argv[2]);
+}
+
+(async() => {
+    await fetchSecrets(true);
+})();
--- index.js
+++ index.js
@@ -2,6 +2,8 @@
  * @file This is what gets imported when a user imports this module in source
  */
 
-module.exports = {
-    // TODO
-};
+import _fetchSecrets from './lib/fetch-secrets.js';
+import _loadSecrets from './lib/load-secrets.js';
+
+export const fetchSecrets = _fetchSecrets;
+export const loadSecrets = _loadSecrets;
